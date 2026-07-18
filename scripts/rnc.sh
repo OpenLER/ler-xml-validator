41e3302ad79d212797e796958d2db236a7863884
Thomas Wessel <mrvelle@gmail.com>
2026-07-18 11:24:39 +0200

    deleted obsolete scripts

--- scripts/rnc.sh
+++
@@ -1,9 +0,0 @@
-export INCLUDE_XSL=/home/thlw/a/schematron/trunk/schematron/code/iso_dsdl_include.xsl
-
-export SCH_RNC=/home/thlw/a/schema/schematron.rnc
-export SVRL_RNC=/home/thlw/a/schema/svrl.rnc
-
-export SCH_SCH=/home/thlw/a/schema/schematron.sch
-export SVRL_SCH=/home/thlw/a/schema/svrl.sch
-
-xsltproc $INCLUDE_XSL main.sch > expanded.sch
